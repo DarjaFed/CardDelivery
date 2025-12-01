5fabbb3ed51c73544ba48438953a74fdf5d6dc2f
DarjaF <carrot.the.oblivion@gmail.com>
2025-12-01 19:55:00 +0300

    first commit

--- src/test/java/ru/netology/web/CardDeliveryNegativeTest.java
+++ src/test/java/ru/netology/web/CardDeliveryNegativeTest.java
@@ -6,12 +6,12 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
+import java.time.Period;
 import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 import static java.time.Duration.ofSeconds;
 import static com.codeborne.selenide.Condition.text;
 
@@ -19,56 +19,68 @@ public class CardDeliveryNegativeTest {
 
     @BeforeEach
     public void setUp() {
-        Selenide.open("http://localhost:9999/");
+        open("http://localhost:9999/");
 
     }
-// Пустое поле города
+
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now()
+                .plusDays(addDays)
+                .format(DateTimeFormatter.ofPattern(pattern));
+    }
+
+
+    // Пустое поле города
     @Test
-    public void shouldReturnErrorMessageIfCityEmpty() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
+    public void noCityName() {
         $("[data-test-id=city] input").setValue("");
-        $("[data-test-id='date'] input");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("+79811111111");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content")
+        $(".notification__content");
+        $("[data-test-id='city'].input_invalid .input__sub").shouldBe(visible)
                 .should(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
+
     //Город не в юрисдикции РФ
     @Test
-    public void shouldReturnErrorMessageIfCityInvalid() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
+    public void CityInvalid() {
         $("[data-test-id=city] input").setValue("Марокко");
-        $("[data-test-id='date'] input");
+        String planningDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("+79811111111");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content")
+        $(".notification__content");
+        $("[data-test-id='city'].input_invalid .input__sub").shouldBe(visible)
                 .should(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(exactText("Доставка в выбранный город недоступна"));
     }
 
-    // Англ.буквы вместо телефона
+    // буквы вместо телефона
     @Test
-    public void shouldReturnErrorMessageIfPhoneWithLetters() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id='date'] input");
+    public void PhoneNumberLetters() {
+        $("[data-test-id=city] input").setValue("Москва");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иван Иванов");
-        $("[data-test-id='phone'] input").setValue("Fgh");
+        $("[data-test-id='phone'] input").setValue("Вав");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content")
+        $(".notification__content");
+        $("[data-test-id='phone'].input_invalid .input__sub").shouldBe(visible)
                 .should(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
@@ -76,101 +88,124 @@ public class CardDeliveryNegativeTest {
 
     // Пустое поле Дата встречи
     @Test
-    public void shouldReturnErrorMessageIfSurnameAndNameIsEmpty() {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id='date'] [class='input__box'] [placeholder='Дата встречи']").doubleClick().sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] [class='input__box'] [placeholder='Дата встречи']").setValue("");
+    public void DateClean() {
+        $("[data-test-id=city] input").setValue("Москва");
+        String planningDate = generateDate(3, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .doubleClick().sendKeys(Keys.BACK_SPACE);
         $("[data-test-id='name'] input").setValue("Иван Иванов");
-        $("[data-test-id='phone'] input").setValue("+79811111111");
+        $("[data-test-id='phone'] input").setValue("+79111111111");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content")
+        $(".notification__content");
+        $("[data-test-id='date'].input_invalid .input__sub")
                 .should(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(exactText("Неверно введена дата"));
+    }
+    @Test
+            public void InvalidDate() {
+    $("[data-test-id=city] input").setValue("Москва");
+    String planningDate = generateDate(3462, "dd.MM.yyyy");
+    $("[data-test-id='date'] input")
+            .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+            .setValue(planningDate);
+    $("[data-test-id='name'] input").setValue("Иван Иванов");
+    $("[data-test-id='phone'] input").setValue("+79111111111");
+    $("[data-test-id='agreement']").click();
+    $("button.button").click();
+    $(".notification__content");
+    $("[data-test-id='date'].input_invalid .input__sub")
+            .should(Condition.visible, Duration.ofSeconds(15))
+            .shouldHave(exactText("Неверно введена дата"));
     }
 
     // Имя английскими буквами
     @Test
-    public void shouldReturnErrorMessageIfInvalidSurnameAndName() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id='date'] input");
+    public void InvalidName() {
+        $("[data-test-id=city] input").setValue("Москва");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Ivan Ivanov");
         $("[data-test-id='phone'] input").setValue("+79811111111");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content")
-                .should(Condition.visible, Duration.ofSeconds(15))
+        $(".notification__content");
+        $("[data-test-id='name'].input_invalid .input__sub")
+                .shouldHave(Condition.visible, Duration.ofSeconds(15))
                 .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     // Имя спецсимволами
     @Test
-    public void shouldReturnErrorMessageIfSurnameAndNameConsistsOfNumbers() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id='date'] input");
-        $("[data-test-id='name'] input").setValue("#$#%!");
+    public void SimbolsName() {
+        $("[data-test-id=city] input").setValue("Москва");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("!@@!");
         $("[data-test-id='phone'] input").setValue("+79811111111");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content")
+        $(".notification__content");
+        $("[data-test-id='name'].input_invalid .input__sub")
                 .should(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+                .shouldHave(Condition.exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     // Пустое поле телефона
     @Test
-    public void shouldReturnSuccessIfFieldsAreFilledInCorrectly() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id='date'] input");
+    public void CleanPhone() {
+        $("[data-test-id=city] input").setValue("Москва");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content")
+        $(".notification__content");
+        $("[data-test-id='phone'].input_invalid .input__sub")
                 .should(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(exactText("Поле обязательно для заполнения"));
+                .shouldHave(Condition.exactText("Поле обязательно для заполнения"));
     }
 
     //Телефон без +
     @Test
-    public void shouldReturnErrorMessageIfPhoneIsWrong() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id='date'] input");
+    public void InvalidePhone() {
+        $("[data-test-id=city] input").setValue("Москва");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("79811111111");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
-        $(".notification__content")
+        $(".notification__content");
+        $("[data-test-id='phone'].input_invalid .input__sub")
                 .should(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+                .shouldHave(Condition.exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
+
     //Без галочки согласия на обработку
     @Test
-    public void shouldReturnErrorMessageIfDoNotTick() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
-        $("[data-test-id=city] input").setValue("Краснодар");
-        $("[data-test-id='date'] input");
+    public void CheckboxOff() {
+        $("[data-test-id=city] input").setValue("Москва");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("+79811111111");
-        $("[data-test-id='agreement']").shouldBe(visible);
+        $("[data-test-id='agreement']").doubleClick();
         $("button.button").click();
-        $(".notification__content")
+        $(".notification__content");
+        $("[data-test-id='agreement'].input_invalid .input__sub")
                 .should(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+                .shouldHave(Condition.exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
     }
 }
--- src/test/java/ru/netology/web/CardDeliveryPositiveTest.java
+++ src/test/java/ru/netology/web/CardDeliveryPositiveTest.java
@@ -6,8 +6,9 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
-import java.time.format.DateTimeFormatter;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$;
@@ -23,52 +24,58 @@ public class CardDeliveryPositiveTest {
         Selenide.open("http://localhost:9999/");
 
     }
+    private String generateDate(long addDays, String pattern) {
+        return LocalDate.now()
+                .plusDays(addDays)
+                .format(DateTimeFormatter.ofPattern(pattern));
+    }
 
     @Test
     public void shouldReturnSuccessIfFieldsAreFilledInCorrectly() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
+
         $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id='date'] input");
+        String planningDate = generateDate(4,"dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("+79811111111");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
                 .should(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " +planningDate));
     }
 
     @Test
     public void shouldReturnSuccessfullyIfSurnameWithHyphen() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
         $("[data-test-id=city] input").setValue("Москва");
-        $("[data-test-id='date'] input");
+        String planningDate = generateDate(6,"dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Иван Иванов-Смирнов");
         $("[data-test-id='phone'] input").setValue("+79811111111");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
                 .should(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " +planningDate));
     }
 
     @Test
     public void shouldReturnSuccessfullyIfCityWithHyphen() {
-        LocalDate deliveryDateCard = LocalDate.now().plusDays(3);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-        String dateText = deliveryDateCard.format(formatter);
         $("[data-test-id=city] input").setValue("Санкт-Петербург");
-        $("[data-test-id='date'] input");
-        $("[data-test-id='name'] input").setValue("Иван Иванов-Смирнов");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input")
+                .press(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE)
+                .setValue(planningDate);
+        $("[data-test-id='name'] input").setValue("Иван Иванов");
         $("[data-test-id='phone'] input").setValue("+79811111111");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $(".notification__content")
                 .should(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + dateText));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
